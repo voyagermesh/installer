714e4121e9d20edff0996cab84f404e4c9e7601c
Tamal Saha <tamal@appscode.com>
2024-09-29 17:14:44 -0700

    Generate image scripts
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ catalog/copy-images.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Copyright AppsCode Inc. and Contributors
+#
+# Licensed under the AppsCode Community License 1.0.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/appscode/licenses/raw/1.0.0/AppsCode-Community-1.0.0.md
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+
+if [ -z "${IMAGE_REGISTRY}" ]; then
+    echo "IMAGE_REGISTRY is not set"
+    exit 1
+fi
+
+OS=$(uname -o)
+if [ "${OS}" = "GNU/Linix" ]; then
+    OS=Linux
+fi
+ARCH=$(uname -m)
+if [ "${ARCH}" = "aarch64" ]; then
+    ARCH=arm64
+fi
+curl -sL "https://github.com/google/go-containerregistry/releases/latest/download/go-containerregistry_${OS}_${ARCH}.tar.gz" >/tmp/go-containerregistry.tar.gz
+tar -zxvf /tmp/go-containerregistry.tar.gz -C /tmp/
+mv /tmp/crane .
+
+CMD="./crane"
+
+$CMD cp ghcr.io/appscode/kubectl:v1.24 $IMAGE_REGISTRY/appscode/kubectl:v1.24
+$CMD cp ghcr.io/voyagermesh/envoy:v1.29.9-ac $IMAGE_REGISTRY/voyagermesh/envoy:v1.29.9-ac
+$CMD cp ghcr.io/voyagermesh/gateway-converter:v0.0.1 $IMAGE_REGISTRY/voyagermesh/gateway-converter:v0.0.1
+$CMD cp ghcr.io/voyagermesh/gateway:v1.1.1 $IMAGE_REGISTRY/voyagermesh/gateway:v1.1.1
+$CMD cp ghcr.io/voyagermesh/voyager:v17.2.0 $IMAGE_REGISTRY/voyagermesh/voyager:v17.2.0
---
+++ catalog/import-images.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright AppsCode Inc. and Contributors
+#
+# Licensed under the AppsCode Community License 1.0.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/appscode/licenses/raw/1.0.0/AppsCode-Community-1.0.0.md
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+
+if [ -z "${IMAGE_REGISTRY}" ]; then
+    echo "IMAGE_REGISTRY is not set"
+    exit 1
+fi
+
+TARBALL=${1:-}
+tar -zxvf $TARBALL
+
+CMD="./crane"
+
+$CMD push images/appscode-kubectl-v1.24.tar $IMAGE_REGISTRY/appscode/kubectl:v1.24
+$CMD push images/voyagermesh-envoy-v1.29.9-ac.tar $IMAGE_REGISTRY/voyagermesh/envoy:v1.29.9-ac
+$CMD push images/voyagermesh-gateway-converter-v0.0.1.tar $IMAGE_REGISTRY/voyagermesh/gateway-converter:v0.0.1
+$CMD push images/voyagermesh-gateway-v1.1.1.tar $IMAGE_REGISTRY/voyagermesh/gateway:v1.1.1
+$CMD push images/voyagermesh-voyager-v17.2.0.tar $IMAGE_REGISTRY/voyagermesh/voyager:v17.2.0
